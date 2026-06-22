961c3a97d19f8adaf329fd51a05960c26ff14987
Jesse Katabarwa <jessekatabarwa@gmail.com>
2026-06-22 20:35:08 +0900

    fixing copy

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ const rows = [
     // Row with a local asset + a Cloudflare R2 asset mixed together
     images: 
     [{ src: 
-      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/Jesse%20Katabarwa_1.jpg',
+      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/Jesse%20Katabarwa_1.jpg', 
        width: '1fr' }],
     title: `2025 ﹅ The Grey Rings of My Eyes`,
     location: 'SSOME.HOW',
@@ -25,17 +25,17 @@ The Grey Rings of My Eyes presentation is an attempt to reappropriate digital sy
 
 images: 
 [{ src: 
-  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/Jesse%20Katabarwa_3.jpg',
+  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/Jesse%20Katabarwa_3.jpg', 
    width: '1fr' },
    { src: 
-  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/Jesse%20Katabarwa_2.jpg',
+  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/Jesse%20Katabarwa_2.jpg', 
    width: '2fr' },
    
   ],
 title: `2025 ﹅ The Grey Rings of My Eyes, mixed media installation, printed matter, transparent film`,
 location: 'SSOME.HOW',
 details: `Emergence is the process and relationship between smaller components interacting with each other to create a larger phenomenon that the isolated parts can't by 
-themselves. That concept can be found in many instances in nature. This is also echoed accross the space, within printed matter, installations, and mixed media art works.`,
+themselves. That concept can be found in many instances in nature. This is echoed accross the space, with printed matter, installations, and mixed media artworks.`,
 backgroundColor: 'var(--reversebg)',
 
 },
@@ -44,11 +44,11 @@ backgroundColor: 'var(--reversebg)',
     images:    
      [         
         { src: 
-      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/diasphora.png',
+      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/diasphora.png', 
        width: '2.2fr' },
 
       { src: 
-      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/V_invite_ssome.how8.jpg',
+      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/V_invite_ssome.how8.jpg', 
        width: '1fr' },
 
   
@@ -89,33 +89,33 @@ images:
 [
 
   //   { src: 
-  // 'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/653634.jpg',
+  // 'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/653634.jpg', 
   //  width: '200px' },
 
 
 
    { src: 
-  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/IMG_6449_c.jpg',
+  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/IMG_6449_c.jpg', 
    width: '0.5fr' },  
   //  { src: 
   // 'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/IMG_6477.MP4',
   //  width: '0.6fr' , type:'video'},
    
    { src: 
-  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh3.1.jpg',
+  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh3.1.jpg', 
    width: '1fr' },
   //  { src: 
-  //     'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh4.png',
+  //     'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh4.png', 
   //      width: '1fr' },
        { src: 
-  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh34.png',
+  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh34.png', 
    width: '1fr' },
    
   ],
 title: `Left: The Engineer 36" by 36" High Saturation Color Ink on Archival Exhibition Paper. Right: Concept Glass & Moss Tables` ,
 location: 'SSOME.HOW',
 details: `Emergence is the process and relationship between smaller components interacting with each other to create a larger phenomenon that the isolated parts can't by 
-themselves. That concept can be found in many instances in nature. This is also echoed accross the space, within printed matter, installations, and mixed media art works.`,
+themselves. That concept can be found in many instances in nature. This is echoed accross the space, within printed matter, installations, and mixed media artworks.`,
 backgroundColor: 'var(--bg)',
 
 },
@@ -124,10 +124,10 @@ backgroundColor: 'var(--bg)',
 images: 
 [
    { src: 
-  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/34141.jpg',
+  'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/34141.jpg', 
    width: '1fr' },
    { src: 
-      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/IMG_6463.jpg',
+      'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/IMG_6463.jpg', 
        width: '1fr' },
        { src: 
   'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/SSH_behindglass.mp4',
@@ -139,7 +139,7 @@ location: 'SSOME.HOW',
 details: `The Grey Rings of My Eyes presentation is an attempt to reappropriate digital systems as a way to understand their humanity/de-humanity. 
  Offering a narrative exploring the relational dynamics of such technology within our public and personal life. The internet as a "public" square but
   also as an intimate nest. Navigating the structural powers threatening and blurring what is public and private.  ⋆ ⊹ ⋆ ⊹ Emergence is the process and relationship between smaller components interacting with each other to create a larger phenomenon that the isolated parts can't by 
-themselves. That concept can be found in many instances in nature. This is also echoed accross the space, within printed matter, installations, and mixed media art works.`,
+themselves. That concept can be found in many instances in nature. This is echoed accross the space, within printed matter, installations, and mixed media artworks.`,
 backgroundColor: 'var(--reversebg)',
 
 },
@@ -151,17 +151,17 @@ backgroundColor: 'var(--reversebg)',
 // images: 
 // [
 //    { src: 
-//   'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/65363.jpg',
+//   'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/65363.jpg', 
 //    width: '1fr' },
 //    { src: 
-//       'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/8356.jpg',
+//       'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/8356.jpg', 
 //        width: '1fr' },
 
 //        { src: 
-//       'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh4.png',
+//       'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/ssh4.png', 
 //        width: '1fr' },
 //        { src: 
-//   'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/87643.jpg',
+//   'https://pub-6052c7e633eb41b588583096f731cf26.r2.dev/87643.jpg', 
 //    width: '1fr' },
    
 //   ],
