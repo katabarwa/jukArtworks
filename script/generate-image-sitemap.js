b98fd13c3fff79da9a22b2ae01d5974020860fa6
Jesse Katabarwa <jessekatabarwa@gmail.com>
2026-06-28 20:30:45 +0900

    seo

---
+++ script/generate-image-sitemap.js
@@ -0,0 +1,36 @@
+// scripts/generate-image-sitemap.js
+import { writeFileSync } from 'fs'
+import { rows } from '../src/data/artworks.js'
+
+const SITE_URL = 'https://art.jessekatabarwa.com'
+
+function cleanForXml(str = '') {
+  return str
+    .replace(/<[^>]*>/g, '')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+}
+
+const imageEntries = rows
+  .flatMap(row => row.images)
+  .filter(img => img.type !== 'video')
+  .map(img => `    <image:image>
+      <image:loc>${img.src}</image:loc>
+    </image:image>`)
+  .join('\n')
+
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+  <url>
+    <loc>${SITE_URL}/</loc>
+${imageEntries}
+  </url>
+</urlset>
+`
+
+writeFileSync('public/sitemap.xml', xml)
+console.log(`Sitemap written with ${rows.flatMap(r => r.images).filter(i => i.type !== 'video').length} images.`)
